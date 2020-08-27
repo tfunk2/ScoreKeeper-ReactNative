d128f7f54373e2726747f749d34dfc9e3deeef8b
tfunk2 <tyler.j.funk2@gmail.com>
2020-08-26 20:11:45 -0600

    expo client updated, initial styiling updated

--- App.js
+++ App.js
@@ -4,13 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <Text>ScoreKeeper</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'maroon'
+    backgroundColor: 'maroon',
+    flex: 1,
+    alignItems: 'center',
+    paddingTop: 20
   }
 });
